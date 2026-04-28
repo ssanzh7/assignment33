f5f94ceaae4e5a404a3af082e2e609b863067d37
Sanzhar Serikov <ssanzh930@gmail.com>
2026-04-28 21:58:57 +0500

    added performance measurement

--- src/main.java
+++ src/main.java
@@ -1,2 +1,7 @@
 public class main {
+    public static void main(String[] args) {
+        experiment experiment = new experiment();
+        System.out.println("Starting Sorting and Searching Analysis...");
+        experiment.runAllExperiments();
+    }
 }
